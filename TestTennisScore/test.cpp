6b79db321e742b756e2b86235fe1d4b5d3ae43af
Daniel Hayes <danihaye@gmail.com>
2021-07-17 08:55:11 -0400

    Adding tests

---
+++ TestTennisScore/test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+// #include "../TennisScore/Athlete.h"
+// #include "../TennisScore/GameState.h"
+// #include "../TennisScore/Utils.h"
+// #include "../TennisScore/TerminalColors.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+// TEST(AthleteTest, OperatorLT)
+// {
+// 	Athlete a{ "a" };
+// 	Athlete b{ "b" };
+// 	EXPECT_LT(a, b);
+// }
