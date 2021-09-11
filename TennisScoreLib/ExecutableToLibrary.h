165cd9fa5972495df757c88597d6f63782e0d693
Daniel Hayes <danihaye@gmail.com>
2021-09-11 18:08:15 -0400

    adding testing suite

---
+++ TennisScoreLib/ExecutableToLibrary.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "../TennisScore/Athlete.h"
+#include "../TennisScore/GameState.h"
+#include "../TennisScore/TerminalColors.h"
+#include "../TennisScore/Utils.h"
--- TestTennisScore/test.cpp
+++ TestTennisScore/test.cpp
@@ -1,17 +1,15 @@
 #include "pch.h"
-// #include "../TennisScore/Athlete.h"
-// #include "../TennisScore/GameState.h"
-// #include "../TennisScore/Utils.h"
-// #include "../TennisScore/TerminalColors.h"
-
+#include "../TennisScoreLib/Athlete.h"
+#include "../TennisScoreLib/GameState.h"
+#include "../TennisScoreLib/TerminalColors.h"
+#include "../TennisScoreLib/Utils.h"
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
 
-// TEST(AthleteTest, OperatorLT)
+// TEST(TestAthlete, TestAthleteConstructor)
 // {
-// 	Athlete a{ "a" };
-// 	Athlete b{ "b" };
-// 	EXPECT_LT(a, b);
+// 	Athlete a{};
+// 	EXPECT_EQ(a.name, "Athlete");
 // }
